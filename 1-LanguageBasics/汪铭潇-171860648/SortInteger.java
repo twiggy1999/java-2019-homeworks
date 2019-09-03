f5f02d2b6a063cc148fcf299a2fc87934f8d1703
WMXsixsixsix <1204260223@qq.com>
2019-09-03 12:40:44 +0800

    delete thread

--- 1-LanguageBasics/汪铭潇-171860648/SortInteger.java
+++ 1-LanguageBasics/汪铭潇-171860648/SortInteger.java
@@ -11,13 +11,13 @@ public class SortInteger {
     public static void main(String[] args){
         int[] array = {5,2,8,4,6,9,1,7,3,0};
         System.out.print("Original array\n");
-        print(array);
-        System.out.print("After sorting(from small to large)\n");
-        sort(array);
-        print(array);
-    }
 
-    private static void sort(int[]array) {
+        System.out.print("Start printing\n");
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i]+" ");
+        System.out.print("\nFinish printing\n");
+
+        System.out.print("After sorting(from small to large)\n");
         int temp = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
@@ -28,12 +28,11 @@ public class SortInteger {
                 }
             }
         }
-    }
 
-    private static void print(int[]array){
         System.out.print("Start printing\n");
         for (int i = 0; i < array.length; i++)
             System.out.print(array[i]+" ");
         System.out.print("\nFinish printing\n");
+
     }
 }
