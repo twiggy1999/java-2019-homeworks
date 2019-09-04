b369731daf0d4a39d3cf148f1454bb6f35b7badc
jianjiang <591584644@qq.com>
2019-09-04 12:40:52 +0800

    homework-1

--- 1-LanguageBasics/张昊-171830544/ArraySort.java
+++ 1-LanguageBasics/张昊-171830544/ArraySort.java
@@ -1,21 +1,25 @@
-package ArraySort;
+//package ArraySort;
+import java.util.Arrays;
 
 public class ArraySort {
-    public static void main(string[] args){
-        int[] array={-2,484,-13,21,234,46,654,6465,654,564};
-        //sort(array,array.length);
-        System.out.println(array.toString());
+    public static void main(String[] args){
+        int[] array={-2,484,-13,21,234,46,653,6465,654,564};
+        sort(array,array.length);
+        System.out.println(Arrays.toString(array));
     }
 
-    /*public static void sort(int[] array,int length) {
-        for(int i = 0;i < length-1;i++) {
-            for (int j = i + 1; j < length; j++) {
-                if (array[i] < array[j]) {
-                    int tempt = array[i];
-                    array[i] = array[j];
-                    array[j] = tempt;
+    public static void sort(int[] array,int length) {
+        for(int i=0;i<length-1;i++)
+        {
+            for(int j=i+1;j<length;j++)
+            {
+                if(array[i]<array[j])
+                {
+                    int temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
                 }
             }
         }
-    }*/
+    }
 }
