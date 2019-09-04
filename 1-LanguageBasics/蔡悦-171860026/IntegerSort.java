4728e092a47b255dae2d87b918391563bdfb3518
Caiy-c <2541033131@qq.com>
2019-09-04 21:14:46 +0800

    homework 1

--- 1-LanguageBasics/蔡悦-171860026/IntegerSort.java
+++ 1-LanguageBasics/蔡悦-171860026/IntegerSort.java
@@ -1,16 +1,14 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 public class IntegerSort {
     public static void main(String[] args) {
         int nums[];
         nums=new int[5];
         System.out.println("Please input five integers:");
-        for(int i=0;i<5;i++)
-        {
-            Scanner in=new Scanner(System.in);
-            nums[i]=in.nextInt();
-        }
+        Scanner si=new Scanner(System.in);
+       for(int i=0;i<5;i++)
+            nums[i]=si.nextInt();
         Arrays.sort(nums);
         for(int i=0;i<5;i++)
         {
