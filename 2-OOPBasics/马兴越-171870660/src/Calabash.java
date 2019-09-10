f9eec8d5624c3513d5c39fe2587d953383a8d1ef
CDK6182CHR <794288384@qq.com>
2019-09-10 20:52:37 +0800

    2019.09.10  first commit for homework 2.

---
+++ 2-OOPBasics/马兴越-171870660/src/Calabash.java
@@ -0,0 +1,56 @@
+/*
+ * order,color皆以1-7表示，数值相同。设为两个变量，预留扩展可能性。
+ */
+public class Calabash {
+    private int order,color;//排行
+
+    public Calabash(int order){
+        this.order=order;
+        this.color=order;
+    }
+
+    public int getOrder(){
+        return order;
+    }
+
+    public String getName(){
+        switch (order){
+            case 1:return "老大";
+            case 2:return "老二";
+            case 3:return "老三";
+            case 4:return "老四";
+            case 5:return "老五";
+            case 6:return "老六";
+            case 7:return "老七";
+            default:return "Invalid order!";
+        }
+    }
+
+    public int getColor(){
+        return color;
+    }
+
+    public String getColorName(){
+        switch (color){
+            case 1:return "红色";
+            case 2:return "橙色";
+            case 3:return "黄色";
+            case 4:return "绿色";
+            case 5:return "青色";
+            case 6:return "蓝色";
+            case 7:return "紫色";
+            default:return "Invalid color!";
+        }
+    }
+
+    /*
+     * 相当于重载小于操作符。按排行比较，当小于时返回true。
+     */
+    public boolean orderBefore(Calabash c){
+        return order<c.order;
+    }
+
+    public boolean colorBefore(Calabash c){
+        return color<c.color;
+    }
+}
---
+++ 2-OOPBasics/马兴越-171870660/src/CalabashBrothers.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+public class CalabashBrothers {
+    private List<Calabash> members;
+    public CalabashBrothers(){
+        members=new ArrayList<>(7);
+        for(int i=0;i<7;i++){
+            members.add(new Calabash(i+1));
+        }
+    }
+
+    /*随意站队*/
+    public void standRandomly(){
+        Collections.shuffle(members);
+    }
+
+    /*按排行冒泡法排序*/
+    public void sortByOrder(){
+        int n=members.size();
+        boolean flag;//标志某一轮是否交换过
+        for(int i=0;i<n-1;i++){
+            flag=false;
+            for(int j=i+1;j<n;j++){
+                if(members.get(j).orderBefore(members.get(i))) {//j<i
+                    swap(i, j);
+                    flag = true;
+                }
+            }
+            if(!flag)
+                break;
+        }
+    }
+
+    /*
+     * 交换i,j位置的元素，同时报告。
+     * 报告时的位置序列是1~7。
+     */
+    private void swap(int i,int j){
+        Calabash temp=members.get(i);
+        members.set(i,members.get(j));
+        members.set(j,temp);
+        System.out.println(members.get(j).getName()+":"+(i+1)+"->"+(j+1));
+        System.out.println(members.get(i).getName()+":"+(j+1)+"->"+(i+1));
+    }
+
+    public void tellOrders(){
+        System.out.print("按排行报数:");
+        for (Calabash member : members) {
+            System.out.print(member.getName() + " ");
+        }
+        System.out.println("");
+    }
+
+    /*
+     * 按颜色进行二分法排序。从末端开始，每次将最后一个元素插入前面。
+     */
+    public void sortByColor(){
+        int n=members.size();
+        for(int i=1;i<n;i++){
+            insert(i,members.get(i));
+        }
+    }
+
+    /*
+     * 二分找到要插入的位置，插入并移动数组元素，同时报告。
+     * 查找[最后一个比c小的位置]
+     * 有解区间[left,right)
+     * precondition: 不会出现相等元素；下标为end的位置原来放的就是c。
+     */
+    private void insert(int end, Calabash c){
+        int left=0,right=end;
+        if(c.colorBefore(members.get(left)))
+            left=-1;//插入位置为0
+        else if(members.get(end-1).colorBefore(c))
+            return;
+        else
+            /*
+             * loop invariant:
+             * members[left]<c, members[right-1]>c
+             * 有解区间：[left,right)
+             */
+            while(right-left>1){
+                int mid=(left+right)/2;
+                if(c.colorBefore(members.get(mid)))
+                    right=mid;
+                else
+                    left=mid;
+            }
+        // post condition: [left+1] is the right place to be inserted.
+        for(int i=end;i>left+1;i--){
+            members.set(i,members.get(i-1));
+            System.out.println(members.get(i).getName()+":"+i+"->"+(i+1));
+        }
+        members.set(left+1,c);
+        System.out.println(c.getName()+":"+(end+1)+"->"+(left+2));
+    }
+
+    public void tellColors(){
+        System.out.print("按颜色报告: ");
+        for (Calabash member : members) {
+            System.out.print(member.getColorName()+" ");
+        }
+        System.out.print("\n");
+    }
+
+}
