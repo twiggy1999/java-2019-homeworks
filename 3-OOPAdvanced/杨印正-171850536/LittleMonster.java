0e79d65934ac31bfb3c061ece4c9634f403c412d
汪浩港 <whg19962015@163.com>
2019-10-13 17:08:54 +0800

    Revert "zxc"

--- 3-OOPAdvanced/杨印正-171850536/LittleMonster.java
+++
@@ -1,9 +0,0 @@
-public class LittleMonster extends LivingBody {//小喽啰类
-    public LittleMonster(int locationx,int locationy,String name)
-    {  super(locationx,locationy,name);
-
-
-    }
-}
-
-
--- 3-OOPAdvanced/杨印正-171850536/ScorpionMonster.java
+++
@@ -1,8 +0,0 @@
-public class ScorpionMonster extends LivingBody{//蝎子类
-    public ScorpionMonster(int locationx,int locationy,String name)
-    {
-        super(locationx,locationy,name);
-    }
-}
-
-
