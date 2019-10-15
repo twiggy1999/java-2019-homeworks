83b8ef2b3feb03a1d1239400189ef6e3de3bef4c
Solomonwisdom <whg19962015@163.com>
2019-10-15 13:26:02 +0800

    杨印正-171850536 hw3

---
+++ 3-OOPAdvanced/杨印正-171850536/ScorpionMonster.java
@@ -0,0 +1,8 @@
+public class ScorpionMonster extends LivingBody{//蝎子类
+    public ScorpionMonster(int locationx,int locationy,String name)
+    {
+        super(locationx,locationy,name);
+    }
+}
+
+
