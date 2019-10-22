66cf62ecee7e84a104d60b42d61af8aeeee708a2
Shen Tianqi <ShenTianqi@users.noreply.github.com>
2019-10-22 21:35:41 +0800

    hw4

---
+++ 4-Types/沈天琪-171860522/src/Monsters.java
@@ -0,0 +1,41 @@
+public class Monsters implements IBeing {
+    private int rank = -1;
+    private String name = "Monsters";
+
+    Monsters(int r) {
+        rank = r;
+    }
+
+    public void print() {
+        System.out.print('M');
+    }
+
+    public void walkTo(int x, int y) {
+        int x1 = BattleField.getXByNo(rank);
+        int y1 = BattleField.getYByNo(rank);
+        if (x1 == x && y1 == y)
+            return;
+        if (x1 == -1) {
+            System.out.println("failed to find myself");
+            return;
+        }
+        if (x1 != x && y1 != y) {
+            System.out.println("can't walk obliquely");
+            return;
+        }
+        if (BattleField.getNo(x, y) != 0) {
+            System.out.println("target position is not empty");
+            return;
+        }
+        BattleField.setField(x, y, rank);
+        BattleField.setField(x1, y1, 0);
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
