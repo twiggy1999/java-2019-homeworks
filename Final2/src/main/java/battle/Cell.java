8192f4f1d72d807a6f4bf50c18c4c839a2380f7f
Mizar77 <35760171+Mizar77@users.noreply.github.com>
2019-12-25 14:35:30 +0800

    Delete Cell.java

--- Final2/src/main/java/battle/Cell.java
+++
@@ -1,9 +0,0 @@
-package battle;
-
-import creature.Creature;
-
-public class Cell {
-    private Creature creature;
-    public Creature getCreature(){return creature;}
-    public void setCreature(Creature c){creature = c;}
-}
