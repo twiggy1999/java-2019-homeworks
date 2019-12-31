1ae86c5abdd459a932f62dabc70559cf48abec6f
jsy-ics <55011290+jsy-ics@users.noreply.github.com>
2019-12-31 21:21:10 +0800

    Add files via upload

---
+++ 4-Types/居思远-171860606/src/Creature/Scorpion.java
@@ -0,0 +1,12 @@
+package Creature;
+
+public class Scorpion extends Creature {
+    public Scorpion(int id)
+    {
+        setId(id);
+        setName("蝎子精");
+        setCamp(1);
+        alive = true;
+        rush = false;
+    }
+}
---
+++ 4-Types/居思远-171860606/src/Creature/Snake.java
@@ -0,0 +1,12 @@
+package Creature;
+
+public class Snake extends Creature {
+
+    public Snake(int id)
+    {
+        setId(id);
+        setName("蛇精");
+        setCamp(1);
+        alive = true;
+    }
+}
